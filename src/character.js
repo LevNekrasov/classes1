2af05c1fed0efe42b4f05becf1f208c9a07587c3
Lev <ya.abc2001@ya.ru>
2023-07-20 01:38:03 +0500

    corrected commitе

--- src/character.js
+++ src/character.js
@@ -4,14 +4,17 @@
 export default class Character {
   constructor(name, type) {
     const gameType = ['Bowerman', 'Daemon', 'Magician', 'Swordsman', 'Undead', 'Zombie'];
-    if (name.length < 2 || name.length > 10 || gameType.find((el) => el === type) === undefined) {
-      throw new Error('Неверно переданы данные');
+    if (name.length < 2 || name.length > 10) {
+      throw new Error('Неверно передано имя персонажа');
+    }
+    if (!gameType.includes(type)) {
+      throw new Error('Неверно передан тип персонажа');
     }
     this.name = name;
     this.health = 100;
     this.level = 1;
-    this.attack = 0;
-    this.defence = 0;
+    this.attack = undefined;
+    this.defence = undefined;
     this.type = type;
   }
 }
