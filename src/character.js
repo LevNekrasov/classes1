f04c0eaa2fb26a08c10b440e79530a12ed9d9f44
LevNekrasov <119606254+LevNekrasov@users.noreply.github.com>
2023-07-19 10:49:17 +0500

    Update character.js

--- src/character.js
+++ src/character.js
@@ -4,8 +4,10 @@
 export default class Character {
   constructor(name, type) {
     const gameType = ['Bowerman', 'Daemon', 'Magician', 'Swordsman', 'Undead', 'Zombie'];
-    if (name.length < 2 || name.length > 10 || gameType.find((el) => el === type) === undefined) {
-      throw new Error('Неверно переданы данные');
+    if (name.length < 2 || name.length > 10) {
+      throw new Error('Неверно передано имя персонажа');} 
+if (!gameType.includes(type)) {
+      throw new Error('Неверно передан тип персонажа');
     }
     this.name = name;
     this.health = 100;
